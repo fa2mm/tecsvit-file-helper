18327b047bf741d9e6ac226542bcc81fcf33a780
fa2m <fa2m@ua.fm>
2017-11-30 12:36:28 +0200

    init

---
+++ FileHelper.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace \tecsvit;
+
+/**
+ * This is just an example.
+ */
+class FileHelper
+{
+    public static $errorInstance;
+
+    public function __construct()
+    {
+        self::$errorInstance = new PublicErrors();
+    }
+
+    /**
+     * Creates a new directory.
+     *
+     * This method is similar to the PHP `mkdir()` function except that
+     * it uses `chmod()` to set the permission of the created directory
+     * in order to avoid the impact of the `umask` setting.
+     *
+     * @param string $path path of the directory to be created.
+     * @param integer $mode the permission to be set for the created directory.
+     * @param boolean $recursive whether to create parent directories if they do not exist.
+     * @return boolean whether the directory is created successfully
+     */
+    public function createDirectory($path, $mode = 0775, $recursive = true)
+    {
+        if (is_dir($path)) {
+            return true;
+        }
+        $parentDir = dirname($path);
+        // recurse if parent dir does not exist and we are not at the root of the file system.
+        if ($recursive && !is_dir($parentDir) && $parentDir !== $path) {
+            static::createDirectory($parentDir, $mode, true);
+        }
+        try {
+            if (!mkdir($path, $mode)) {
+                return false;
+            }
+        } catch (\Exception $e) {
+            if (!is_dir($path)) {// https://github.com/yiisoft/yii2/issues/9288
+                self::$errorInstance->addError('Failed to create directory "'. $path. '": ' . $e->getMessage());
+
+                return false;
+            }
+        }
+        try {
+            return chmod($path, $mode);
+        } catch (\Exception $e) {
+            self::$errorInstance->addError(
+                'Failed to change permissions for directory "' . $path . '": ' . $e->getMessage()
+            );
+
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+File helper
+===========
+File helper
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist tecsvit/file-helper "*"
+```
+
+or add
+
+```
+"tecsvit/file-helper": "*"
+```
+
+to the require section of your `composer.json` file.
