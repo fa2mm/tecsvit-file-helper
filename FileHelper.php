e25af91429ee003779040a2cfc952701317d976f
fa2m <fa2m@ua.fm>
2017-11-30 16:02:38 +0200

    refactoring

--- FileHelper.php
+++ FileHelper.php
@@ -2,16 +2,46 @@
 
 namespace \tecsvit;
 
+use \Exception;
+
 /**
- * This is just an example.
+ * Class FileHelper
+ *
+ * Date: 30.11.2017
+ *
+ * @static PublicErrors $errorInstance
+ *
+ * @use \tecsvit\PublicErrors
  */
 class FileHelper
 {
     public static $errorInstance;
 
-    public function __construct()
+    /**
+     * @return void
+     */
+    public static function initErrors()
     {
-        self::$errorInstance = new PublicErrors();
+        if (null === self::$errorInstance) {
+            self::$errorInstance = new PublicErrors();
+        }
+    }
+
+    /**
+     * @return PublicErrors
+     */
+    public static function errorInstance()
+    {
+        self::initErrors();
+        return self::$errorInstance;
+    }
+
+    /**
+     * @return FileHelper
+     */
+    public static function init()
+    {
+        return new self();
     }
 
     /**
@@ -29,42 +59,39 @@ class FileHelper
     }
 
     /**
-     * Creates a new directory.
-     *
-     * This method is similar to the PHP `mkdir()` function except that
-     * it uses `chmod()` to set the permission of the created directory
-     * in order to avoid the impact of the `umask` setting.
-     *
-     * @param string $path path of the directory to be created.
-     * @param integer $mode the permission to be set for the created directory.
-     * @param boolean $recursive whether to create parent directories if they do not exist.
-     * @return boolean whether the directory is created successfully
+     * @param string    $path
+     * @param integer   $mode
+     * @param boolean   $recursive
+     * @return boolean  whether
      */
-    public static function createDirectory($path, $mode = 0775, $recursive = true)
+    public function createDirectory($path, $mode = 0775, $recursive = true)
     {
         if (is_dir($path)) {
             return true;
         }
+
         $parentDir = dirname($path);
-        // recurse if parent dir does not exist and we are not at the root of the file system.
+
         if ($recursive && !is_dir($parentDir) && $parentDir !== $path) {
-            static::createDirectory($parentDir, $mode, true);
+            $this->createDirectory($parentDir, $mode, true);
         }
+
         try {
             if (!mkdir($path, $mode)) {
                 return false;
             }
-        } catch (\Exception $e) {
-            if (!is_dir($path)) {// https://github.com/yiisoft/yii2/issues/9288
-                self::$errorInstance->addError('Failed to create directory "'. $path. '": ' . $e->getMessage());
+        } catch (Exception $e) {
+            if (!is_dir($path)) {
+                self::errorInstance()->addError('Failed to create directory "'. $path. '": ' . $e->getMessage());
 
                 return false;
             }
         }
+
         try {
             return chmod($path, $mode);
-        } catch (\Exception $e) {
-            self::$errorInstance->addError(
+        } catch (Exception $e) {
+            self::errorInstance()->addError(
                 'Failed to change permissions for directory "' . $path . '": ' . $e->getMessage()
             );
 
